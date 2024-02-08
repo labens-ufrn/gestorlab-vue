9b84f1ff407390d8b7b2f60eff3a08e178a751d3
JFmaia.dev <jfmaia.dev@gmail.com>
2024-02-08 20:48:57 -0300

    feat: Pratic typescript with vue!

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <script setup lang="ts">
 // import { RouterLink, RouterView } from 'vue-router'
+
+interface PersonInterface {
+  id: number,
+  name: string,
+  age: number,
+}
+
+let persons:Array<PersonInterface> = [
+  {
+    id: 1,
+    name: 'Jose',
+    age: 23
+  },
+  {
+    id: 2,
+    name: 'Maria',
+    age: 41
+  },
+  {
+    id: 3,
+    name: 'Danyélica',
+    age: 25
+  },
+];
 </script>
 
 <template>
-  <h1>Vue with ts</h1>
+  <h1>My list of users</h1>
+  <ul>
+    <li v-for="person in persons" :key="person.id">
+      {{ person.name }}
+    </li>
+  </ul>
 </template>
 
 <style scoped>
