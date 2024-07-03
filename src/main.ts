e0fd7e0391744a7bb7b96d8e7cab58e1264928bd
JFmaia <jfmaia.dev@gmail.com>
2024-07-03 15:42:30 -0300

    feat: Modified main (#1)

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
-const app = createApp(App);
-app.use(createPinia());
-app.use(router);
+const pinia = createPinia();
 
+const app = createApp(App);
+app.use(pinia);
+app.use(router);
 app.mount('#app');
