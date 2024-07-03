6e70da72b32d059a0a94c52abf3df8cdf91bc2c1
JFmaia <jfmaia.dev@gmail.com>
2024-07-03 15:44:20 -0300

    feat: Create new state in storePinia (#1)

---
+++ src/stores/auth.ts
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia';
+import API from '@/services/index';
+
+export const authStore = defineStore('auth', {
+  state: () => ({
+    token: '',
+  }),
+  actions: {
+    async authLogin(formData: FormData) {
+      try {
+        const response = await API.post('/usuarios/login', formData, {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        });
+        const token = response.data.access_token;
+        document.cookie = token;
+        return true;
+      } catch (err) {
+        return false;
+      }
+    }
+  }
+});
