e155d9c5eef49de36de7b0c53babced8d2b4631e
JFmaia <jfmaia.dev@gmail.com>
2024-07-05 13:17:53 -0300

    feat: Retired console.log (#1)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
 export function removerCaracter(texto: any) {
   const regex = /[.\-\/()\s]/g;
   const textoSemCaracteres = texto.replace(regex, '');
-  console.log(textoSemCaracteres);
   return Number(textoSemCaracteres);
 }
