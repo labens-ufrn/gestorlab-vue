950d1167fd28a4119282c1d63baced9df604c58f
JFmaia <jfmaia.dev@gmail.com>
2024-07-09 10:46:11 -0300

    feat: Created new loadingPage (#1)

---
+++ src/components/LoadingPage.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { QSpinner } from 'quasar';
+</script>
+
+<template>
+  <div class="loading-page">
+    <QSpinner
+      size="30px"
+      color="primary"
+    />
+  </div>
+</template>
+
+<style scoped>
+  .loading-page {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
