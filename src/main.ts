51d9bf701a43de243c24687ef142ce687947bdfa
JFmaia <jfmaia.dev@gmail.com>
2024-07-16 16:28:55 -0300

    feat: Modified file (#5)

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,8 @@ import router from './router';
 import VueTheMask from 'vue-the-mask';
 import quasarPlugin from './plugins/quasar';
 
-const pinia = createPinia();
-
 const app = createApp(App);
+const pinia = createPinia();
 (app as any).use(VueTheMask);
 app.use(quasarPlugin);
 app.use(pinia);
