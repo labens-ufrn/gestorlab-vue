71fb8397c28f812206534a81d7c18786bb19850b
JFmaia <jfmaia.dev@gmail.com>
2024-07-30 16:33:36 -0300

    feat: Created new component of card (#11)

---
+++ src/components/MyCard.vue
@@ -0,0 +1,123 @@
+<script setup lang="ts">
+import {ref, onMounted} from 'vue';
+import { QCard, QSeparator, QCardSection, QSpinnerDots} from 'quasar';
+// props
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+defineProps({
+  title: {
+    type: String,
+    required: true,
+    default: 'Title'
+  },
+  subTitle: {
+    type: String,
+    required: true,
+    default: 'Coordenador'
+   
+  },
+  summary: {
+    type: String,
+    required: true,
+    default: 'Laboratório voltado para o desenvolvimento de software e banco de dados relacional!aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+  }
+});
+
+// variables 
+let loading = ref<Boolean>(false);
+
+// function
+onMounted(async()=> {
+  console.log('Init Component');
+});
+</script>
+<template>
+  <q-card
+    dark
+    bordered
+    class="my-card"
+  >
+    <q-card-section>
+      <article>
+        <h1>{{ title }}</h1>
+        <p>{{ subTitle }}</p>
+      </article>
+    </q-card-section>
+    <q-separator
+      dark
+    />
+    <q-card-section>
+      <article>
+        <span>{{ summary }}</span>
+        <button type="submit">
+          <QSpinnerDots
+            size="20px"
+            color="dark"
+            v-if="loading"
+          />
+          <template v-else>
+            Se candidatar
+          </template>
+        </button>
+      </article>
+    </q-card-section>
+  </q-card>
+</template>
+<style scoped lang="scss">
+  .my-card {
+    border: 1px solid $contour;
+    box-shadow: 0px 4px 4px 0px $dark;
+    width: 20rem;
+    height: 22rem;
+    background-color: $dark;
+  }
+  article {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1.2rem;
+  }
+
+  article h1 {
+    line-height: 2rem;
+    max-width: 20rem;
+    max-height: 4rem;
+    font-size: 1.3rem;
+    font-weight: 700;
+    color: $textColor;
+  }
+
+  article p {
+    font-size: 1rem;
+    max-height: 4rem;
+    max-width: 20rem;
+    font-weight: 500;
+    font-style: normal;
+    color: $textDisabled;
+  }
+
+  article span {
+    max-width: 18rem;
+    height: 6rem; 
+    font-size: 0.9rem;
+    font-weight: 500;
+    font-style: italic;
+    color: $textColor; 
+    overflow: hidden; 
+    text-overflow: ellipsis; 
+    white-space: normal; 
+  }
+
+  button {
+    width: 100%;
+    border: none;
+    cursor: pointer;
+    border-radius: 10px;
+    padding: 0.7rem;
+    font-size: 0.9rem;
+    font-weight: 600;
+    background-color: $secondary;
+    color: $textColor;
+  }
+</style>
